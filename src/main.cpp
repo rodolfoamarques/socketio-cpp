8da6f8bb4b413fc62cca708507e358feec29029e
Rodolfo Marques <rodolfo.marques@softruck.com>
2016-07-27 23:35:37 -0300

    Updated the main function to use the class to send messages to server.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 
 #include <iostream>
 
+#include "SocketAPI/SocketModel.hpp"
+
 
 int main ( int argc, char *argv[] )
 {
-	std::cout << "Hello World!" << std::endl;
+	/* initiallize the socket connection */
+	SocketModel::init( "http://127.0.0.1:3000", 1 );
+
+	/* send a message to the socket */
+	for( uint32_t i=0; i<10; i++ ) {
+
+		SocketModel::sendMessage(
+			"new message", "hello " + std::to_string(i) );
+	}
+
+	/* close socket connection */
+	SocketModel::cleanUp();
 
 	return 0;
 }
